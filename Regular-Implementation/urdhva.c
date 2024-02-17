ff61b8ea032377038a9610eb7fac7d965fee3f88
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-18 01:24:53 +0530

    Update urdhva.c

--- Regular-Implementation/urdhva.c
+++ Regular-Implementation/urdhva.c
@@ -9,8 +9,8 @@
 #include <gmp.h>
 #include <float.h>
 
-#define NUMBER_OF_BITS 4096
-#define CSV_FILENAME "urdhva_multiplication_results_4096.csv"
+#define NUMBER_OF_BITS 2097152
+#define CSV_FILENAME "urdhva_multiplication_results_2097152.csv"
 
 uint64_t start_ticks, end_ticks;
 uint64_t min_ticks = UINT64_MAX;
