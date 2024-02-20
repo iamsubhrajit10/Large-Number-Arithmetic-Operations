8c78e8b54e2ad3d06cca7ae4506b1ad8cc95fda1
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-20 11:25:39 +0530

    Update intel-thp.c

--- Transparent Huge Pages/intel-thp.c
+++ Transparent Huge Pages/intel-thp.c
@@ -228,11 +228,11 @@ int main() {
         // Print results to the file
         printResultsToFile(results_file, iteration);
         printf("\nDone: Iter%d\n", iteration);
-        madvise(final_result.digits, HPAGE_SIZE, MADV_DONTNEED);
-        madvise(num1.digits, HPAGE_SIZE, MADV_DONTNEED);
-        madvise(num2.digits, HPAGE_SIZE, MADV_DONTNEED);
-        freeBigInteger(num1);
-        freeBigInteger(num2);
+        // madvise(final_result.digits, HPAGE_SIZE, MADV_DONTNEED);
+        // madvise(num1.digits, HPAGE_SIZE, MADV_DONTNEED);
+        // madvise(num2.digits, HPAGE_SIZE, MADV_DONTNEED);
+        freeBigInteger(&num1);
+        freeBigInteger(&num2);
         freeBigInteger(&final_result);
     }
 
