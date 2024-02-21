e0689b8b7b77cf7e41e2fff687fd6b9c5df88baf
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-21 16:38:07 +0530

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install linux-tools-common
+sudo apt install linux-tools-5.15.0-86-generic
+sudo apt update
+sudo apt-get install libgmp3-dev
+git clone https://github.com/iamsubhrajit10/Large-Number-Arithmetic-Operations
