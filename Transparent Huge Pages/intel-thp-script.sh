e2cdbfce111f598f0301198e7dde903eff811624
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-22 03:07:02 +0530

    Update intel-thp-script.sh

--- Transparent Huge Pages/intel-thp-script.sh
+++ Transparent Huge Pages/intel-thp-script.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Number of iterations
-n=10
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <input_source_file> <executable_name> <number_of_bits> <number_of_iterations>"
+    exit 1
+fi
+
+# Extract command-line arguments
+input_source_file="$1"
+executable_name="$2"
+number_of_bits="$3"
+number_of_iterations="$4"
 
 # Array to store tick values
 declare -a ticks
 
 # Compile the C program
-gcc -o intel-thp intel-thp.c -lgmp
+gcc -o "$executable_name" "$input_source_file" -lgmp
 
-# Run the program 'n' times and record tick values
-for ((i=0; i<$n; i++))
+# Run the program 'number_of_iterations' times and record tick values
+for ((i=0; i<$number_of_iterations; i++))
 do
-    # Run the C program and extract the tick value
-    tick=$(./multiply_program <number_of_bits> | grep "Ticks" | awk '{print $2}')
+    # Run the compiled program and extract the tick value
+    tick=$(./"$executable_name" "$number_of_bits" | grep "Ticks" | awk '{print $2}')
     
     # Store the tick value in the array
     ticks[$i]=$tick
@@ -30,7 +39,7 @@ do
         min_ticks=$tick
     fi
 done
-average_ticks=$((total_ticks / n))
+average_ticks=$((total_ticks / number_of_iterations))
 
 # Print results
 echo "Average ticks: $average_ticks"
