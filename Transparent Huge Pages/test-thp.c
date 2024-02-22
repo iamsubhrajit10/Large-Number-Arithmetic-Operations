8f446fe57614eeec70c6f1fb587adfd2c2c9abba
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-23 01:03:28 +0530

    Update test-thp.c

--- Transparent Huge Pages/test-thp.c
+++ Transparent Huge Pages/test-thp.c
@@ -1,105 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <stdint.h>  // Include this header for uint64_t
-#include <stdbool.h>
-#include <sys/mman.h>
+#include <stdint.h>
+#include <limits.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
-#define HPAGE_SIZE (2 * 1024 * 1024)
-#define PAGE_SIZE 4096
+#define HUGEPAGE_SIZE (2 * 1024 * 1024) // 2 MiB
 
-size_t page_index(void *ptr) {
-    return (size_t)ptr / PAGE_SIZE;
+void *thp_malloc(size_t size) {
+    void *ptr;
+    if (size > SIZE_MAX / sizeof(void *)) {
+        return NULL;
+    }
+    if (posix_memalign(&ptr, HUGEPAGE_SIZE, size) != 0) {
+        return NULL;
+    }
+    if (madvise(ptr, size, MADV_HUGEPAGE) != 0) {
+        return NULL;
+    }
+    return ptr;
 }
 
-size_t get_pfn(void *ptr) {
-    size_t pi = page_index(ptr);
-    FILE *fp = fopen("/proc/self/pagemap", "rb");
-
-    if (fp == NULL) {
-        perror("Error opening /proc/self/pagemap");
-        exit(EXIT_FAILURE);
-    }
-
-    // Each entry in pagemap is 64 bits, i.e., 8 bytes.
-    fseek(fp, pi * 8, SEEK_SET);
-
-    uint64_t page_info = 0;
-    size_t result = fread(&page_info, 8, 1, fp);
-    if (result != 1) {
-        perror("Error reading pagemap");
-        exit(EXIT_FAILURE);
-    }
-
-    // Check if the page is present (bit 63). Otherwise, there is no PFN.
-    if (page_info & ((uint64_t)1 << 63)) {
-        uint64_t pfn = page_info & (((uint64_t)1 << 55) - 1);
-
-        if (pfn == 0) {
-            printf("Could not get page frame number. Does the process have the CAP_SYS_ADMIN capability?\n");
-        }
-
-        fclose(fp);
-        return (size_t)pfn;
-    } else {
-        printf("Page not present.\n");
-        fclose(fp);
-        return 0;
-    }
-}
-
-size_t get_pflags(void *ptr) {
-    size_t pfn = get_pfn(ptr);
-    if (pfn == 0) {
-        return 0;
-    }
-
-    FILE *fp = fopen("/proc/kpageflags", "rb");
-    if (fp == NULL) {
-        perror("Error opening /proc/kpageflags");
-        exit(EXIT_FAILURE);
-    }
-
-    fseek(fp, pfn * 8, SEEK_SET);
-
-    uint64_t pflags = 0;
-    size_t result = fread(&pflags, 8, 1, fp);
-    if (result != 1) {
-        perror("Error reading pageflags");
-        exit(EXIT_FAILURE);
-    }
-
-    fclose(fp);
-    return (size_t)pflags;
-}
-
-bool is_ehp(void *ptr) {
-    uint64_t flags = get_pflags(ptr);
-    return (flags & ((uint64_t)1 << 17)); // "huge" flag
-}
-
-bool is_thp(void *ptr) {
-    uint64_t flags = get_pflags(ptr);
-    return (flags & ((uint64_t)1 << 22)); // "transparent huge"
-}
-
-bool is_huge(void *ptr) {
-    return is_ehp(ptr) || is_thp(ptr);
+void thp_free(void *ptr) {
+    free(ptr);
 }
 
 int main() {
-    size_t size = 4 * HPAGE_SIZE;
-    void *mem = aligned_alloc(HPAGE_SIZE, size);
+    // Example usage
+    int *arr = (int *)thp_malloc(100 * sizeof(int));
+    if (arr == NULL) {
+        perror("thp_malloc");
+        return EXIT_FAILURE;
+    }
 
-    madvise(mem, size, MADV_HUGEPAGE);
+    for (int i = 0; i < 100; ++i) {
+        arr[i] = i;
+    }
 
-    // Make sure the page is present
-    ((char *)mem)[0] = 'x';
+    for (int i = 0; i < 100; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
-    printf("Is huge? %d\n", is_huge(mem));
-    printf("Is THP? %d\n", is_thp(mem));
-    long page_size = sysconf(_SC_PAGESIZE);
-    printf("Page size for result.digits: %ld bytes\n", page_size);
-    return 0;
+    thp_free(arr);
+
+    return EXIT_SUCCESS;
 }
