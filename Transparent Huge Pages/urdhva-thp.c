4cb7ad667318660fe2052ac89e5e5f18cb274679
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-22 11:21:24 +0530

    Update urdhva-thp.c

--- Transparent Huge Pages/urdhva-thp.c
+++ Transparent Huge Pages/urdhva-thp.c
@@ -172,13 +172,12 @@ void multiply() {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <No of bits> <No of epochs>\n", argv[0]);
+    if (argc != 2) {
+        printf("Usage: %s <No of bits>\n", argv[0]);
         return 1;
     }
 
     NUMBER_OF_BITS = atoi(argv[1]);
-    int NUMBER_OF_EPOCHS = atoi(argv[2]);
 
     char CSV_FILENAME[100];
     snprintf(CSV_FILENAME, sizeof(CSV_FILENAME), "experiment_intel_multiplication_results_%d.csv", NUMBER_OF_BITS);
