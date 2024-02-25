9c525ff8337d60e10e0208ddb38bafd495e51f06
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-25 22:27:46 +0530

    Update intel-thp.c

--- Transparent Huge Pages/intel-thp.c
+++ Transparent Huge Pages/intel-thp.c
@@ -16,8 +16,8 @@ struct BigInteger final_result;
 struct BigInteger num1;
 struct BigInteger num2;
 int NUMBER_OF_BITS;
-uint64_t start_ticks, end_ticks;
-int iteration;
+uint64_t start_ticks, end_ticks,total_ticks,min_ticks;
+
 
 char* generateRandomNumber(int seed) {
     gmp_randstate_t state;
@@ -186,8 +186,18 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < num1.length + num2.length; ++i) {
         final_result.digits[i] = 0;
     }
-
-    multiply();
+    for (int i =0;i<1000;i++){
+        printf("Iteration %d starting...\n",i);
+        multiply();
+        printf("Iteration %d done...\n",i);
+            // Record the ending ticks
+        total_ticks += (end_ticks - start_ticks);
+    
+        if ((end_ticks - start_ticks) < min_ticks) {
+            min_ticks = (end_ticks - start_ticks);
+        }
+        printf("Average ticks: %f, Min Ticks: %d\n",total_ticks/(i+1),min_ticks);
+    }
 
     // Print results to the file
     printResultsToFile(results_file);
