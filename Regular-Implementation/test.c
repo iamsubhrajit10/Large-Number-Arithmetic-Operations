2875a8006a3e3cf8dd8e72cf946dc601c030e88a
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-02 00:01:04 +0530

    fix

--- Regular-Implementation/test.c
+++ Regular-Implementation/test.c
@@ -17,7 +17,7 @@
 
 #define NUM_DIGITS 100
 #define NUM_ITERATIONS 1
-#define NUMBER_OF_BITS 8192
+#define NUMBER_OF_BITS 256
 #define MAX_EVENTS 11 // Maximum number of events to monitor
 uint64_t start_ticks, end_ticks,total_ticks,min_ticks=UINT64_MAX;
 struct BigInteger *nums,*results;
