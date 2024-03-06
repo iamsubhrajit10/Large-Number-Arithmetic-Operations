bf4ff5a27219b3bdf776eddcff09a42e50d6c28e
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 17:23:37 +0530

    try-fix-karatsuba-thp

--- Transparent Huge Pages/karatsuba_prealloc_thp.c
+++ Transparent Huge Pages/karatsuba_prealloc_thp.c
@@ -110,6 +110,7 @@ void multiply(struct BigInteger *x, struct BigInteger *y, struct BigInteger *res
     // Intermediate results
     //preallocate space for z0, z1, and z2 using thp
     int total_size = (3 * n * 2 + 2 * n * 2) * sizeof(int);
+    printf("Total size: %d\n", total_size);
     int *z_space;
     posix_memalign((void **)&z_space, HPAGE_SIZE, total_size);
     int err = madvise(z_space, total_size, MADV_HUGEPAGE);
