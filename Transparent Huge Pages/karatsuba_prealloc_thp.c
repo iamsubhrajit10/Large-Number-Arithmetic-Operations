ff52d80f315301b5f0737d4a46640ee49d0e6d79
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 17:14:29 +0530

    try-fix-karatsuba-thp

--- Transparent Huge Pages/karatsuba_prealloc_thp.c
+++ Transparent Huge Pages/karatsuba_prealloc_thp.c
@@ -16,8 +16,8 @@
 #include <asm/unistd.h>  // For __NR_perf_event_open
 
 #define NUM_DIGITS 1000
-#define NUM_ITERATIONS 1
-#define NUMBER_OF_BITS 8192
+#define NUM_ITERATIONS 100
+#define NUMBER_OF_BITS 16384
 #define MAX_EVENTS 11 // Maximum number of events to monitor
 #define HPAGE_SIZE (2<<21)
 
@@ -108,10 +108,12 @@ void multiply(struct BigInteger *x, struct BigInteger *y, struct BigInteger *res
     low2.length = n - half;
 
     // Intermediate results
-    //preallocate space for z0, z1, and z2
-    int *z_space = (int *)malloc(3 * n* 2* sizeof(int));
-    if (z_space == NULL) {
-        perror("Memory allocation failed");
+    //preallocate space for z0, z1, and z2 using thp
+    int *z_space;
+    posix_memalign((void **)&z_space, HPAGE_SIZE, 3 * n* 2* sizeof(int));
+    int err = madvise(z_space, 3 * n* 2* sizeof(int), MADV_HUGEPAGE);
+    if (err != 0) {
+        perror("madvise z_space");
         exit(EXIT_FAILURE);
     }
     struct BigInteger z0, z1, z2;
@@ -126,11 +128,12 @@ void multiply(struct BigInteger *x, struct BigInteger *y, struct BigInteger *res
     multiply(&low1, &low2, &z0);
     multiply(&high1, &high2, &z2);
 
-    //preallocate space for low_sum and high_sum
-    int *sum_space = (int *)malloc(2 * n* 2* sizeof(int));
-    if (sum_space == NULL) {
-        perror("Memory allocation failed");
-        free(z_space);
+    //preallocate space for low_sum and high_sum using thp
+    int *sum_space;
+    posix_memalign((void **)&sum_space, HPAGE_SIZE, 2 * n* 2* sizeof(int));
+    err = madvise(sum_space, 2 * n* 2* sizeof(int), MADV_HUGEPAGE);
+    if (err != 0) {
+        perror("madvise sum_space");
         exit(EXIT_FAILURE);
     }
     struct BigInteger low_sum, high_sum;
@@ -182,8 +185,8 @@ void multiply(struct BigInteger *x, struct BigInteger *y, struct BigInteger *res
         }
     }
     // Clean up memory
-    free(z_space);
-    free(sum_space);
+    madvise(z_space, 3 * n* 2* sizeof(int), MADV_DONTNEED);
+    madvise(sum_space, 2 * n* 2* sizeof(int), MADV_DONTNEED);
 }
 
 int main()
@@ -252,7 +255,7 @@ int main()
         }
         results[i].length = length;
     }
-   // Loop to generate random numbers and perform Karatsuba multiplicati
+`   // Loop to generate random numbers and perform Karatsuba multiplicati
     int k=0;
     // Run your code here...
     // printf("Starting the computation for non-thp...\n");
