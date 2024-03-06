45b3235ad3aeea373fc6a5b97bf7fc5de160cca1
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 10:37:22 +0530

    ok

--- grade_school_prealloc_peo_script.sh
+++ grade_school_prealloc_peo_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ./Transparent\ Huge\ Pages
-gcc -o g_s_p_t_p_e_o grade_school_prealloc_thp_perf_event_open.c -lgmp
+gcc -o g_s_p_t_p_e_o grade_school_prealloc_thp_perf_event_open.c -lgmp -o3
 sudo ./g_s_p_t_p_e_o | tee g_s_p_t_p_e_o-dump.txt
 echo "grade_school_prealloc_thp_perf_event_open done"
 
 cd ../Regular-Implementation
-gcc -o g_s_p_p_e_o grade_school_prealloc_perf_event_open.c -lgmp
+gcc -o g_s_p_p_e_o grade_school_prealloc_perf_event_open.c -lgmp -o3
 sudo ./g_s_p_p_e_o | tee g_s_p_p_e_o-dump.txt
 echo "grade_school_prealloc_perf_event_open done"
 
--- grade_school_prealloc_ps_script.sh
+++ grade_school_prealloc_ps_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ./Transparent\ Huge\ Pages
-gcc -o g_s_p_t_p grade_school_prealloc_thp.c -lgmp
+gcc -o g_s_p_t_p grade_school_prealloc_thp.c -lgmp -o3
 sudo perf stat -e cpu-cycles,instructions,branch-misses,faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references ./g_s_p_t_p | tee g_s_p_t_p-dump.txt
 echo "grade_school_prealloc_thp done"
 
 cd ../Regular-Implementation
-gcc -o g_s_p grade_school_prealloc.c -lgmp
+gcc -o g_s_p grade_school_prealloc.c -lgmp -o3
 sudo perf stat -e cpu-cycles,instructions,branch-misses,faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references ./g_s_p | tee g_s_p-dump.txt
 echo "grade_school_prealloc done"
 
