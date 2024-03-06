2589e3687bd4ee83c5838b3f5cdfa0620becbfea
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 11:26:00 +0530

    fix-git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -3,6 +3,10 @@
 while true
 do
   cd ~/Large-Number-Arithmetic-Operation
+  # If nohup.out exists, move it to the home directory
+  if [ -f nohup.out ]; then
+    mv nohup.out ~/
+  fi
   git checkout server_commit_branch || git checkout -b server_commit_branch
   git add .
   git commit -m "server commit"
