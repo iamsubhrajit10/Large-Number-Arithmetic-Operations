f3f1995842f27c04461abbc0751374d19ca39275
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 23:57:51 +0530

    check-karatsuba

--- Transparent Huge Pages/karatsuba_prealloc_thp.c
+++ Transparent Huge Pages/karatsuba_prealloc_thp.c
@@ -7,16 +7,15 @@
 // Splits a number into two parts, handling numbers with odd lengths.
 void split_number(const char *num, char **first, char **second, int n) {
     int half_size = n / 2;
-    int remainder = n % 2; 
 
     *first = malloc(half_size + 1);
-    *second = malloc(half_size + remainder + 1);
+    *second = malloc(n - half_size + 1);
 
     strncpy(*first, num, half_size);
     (*first)[half_size] = '\0'; 
 
-    strncpy(*second, num + half_size, half_size + remainder);
-    (*second)[half_size + remainder] = '\0'; 
+    strncpy(*second, num + half_size, n - half_size);
+    (*second)[n - half_size] = '\0'; 
 }
 
 
