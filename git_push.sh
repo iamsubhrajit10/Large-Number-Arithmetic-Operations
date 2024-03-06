6090a139febcf618a77cedc8ad854b94885aee10
iamsubhrajit10 <133939116+iamsubhrajit10@users.noreply.github.com>
2024-03-06 11:21:30 +0530

    fix-git-psush.sh

--- git_push.sh
+++ git_push.sh
@@ -3,9 +3,9 @@
 while true
 do
   cd ~/Large-Number-Arithmetic-Operation
-  git checkout auto_commit_branch || git checkout -b auto_commit_branch
+  git checkout server_commit_branch || git checkout -b server_commit_branch
   git add .
-  git commit -m "auto commit"
-  git push origin auto_commit_branch
+  git commit -m "server commit"
+  git push origin server_commit_branch
   sleep 1800
 done
