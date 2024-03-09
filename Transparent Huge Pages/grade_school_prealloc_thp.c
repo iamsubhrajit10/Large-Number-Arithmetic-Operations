1082a636b8a2858644aa9781029f31f4ec7e700f
iamsubhrajit10 <iamsubhrajit10@gmail.com>
2024-03-09 05:27:07 +0000

    Refactor computation loop and add index shuffling

--- Transparent Huge Pages/grade_school_prealloc_thp.c
+++ Transparent Huge Pages/grade_school_prealloc_thp.c
@@ -178,15 +178,31 @@ int main() {
     // Run your code here...
 
     // printf("Starting the computation for thp...\n");
-    for (int i = 0; i < NUM_DIGITS; i+=2) {       
+    int indices[NUM_DIGITS];
+    for (int i = 0; i < NUM_DIGITS; i++) {
+        indices[i] = i;
+    }
+    
+    for (int i = 0; i < NUM_DIGITS; i += 2) {
+        // Shuffle the indices array
+        for (int j = NUM_DIGITS - 1; j > 0; j--) {
+            int randomIndex = rand() % (j + 1);
+            int temp = indices[j];
+            indices[j] = indices[randomIndex];
+            indices[randomIndex] = temp;
+        }
+        
+        int index1 = indices[i];
+        int index2 = indices[i + 1];
+        
         // Your computation code goes here...
-        for (int j=0;j<NUM_ITERATIONS;j++){
-            multiply(&nums[i], &nums[i+1], &results[k]);
-            if(end_ticks - start_ticks < min_ticks){
+        for (int j = 0; j < NUM_ITERATIONS; j++) {
+            multiply(&nums[index1], &nums[index2], &results[k]);
+            if (end_ticks - start_ticks < min_ticks) {
                 min_ticks = end_ticks - start_ticks;
             }
             total_ticks += end_ticks - start_ticks;
-            }
+        }
         k++;
     }
     printf("Minimum ticks: %lu\n", min_ticks);
