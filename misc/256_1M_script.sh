52b80039a3af2be18a94cc104b3b23f8f24340de
lattu <lattu@node-1.digitsonturbo.isolateedinburgh-pg0.clemson.cloudlab.us>
2024-08-28 11:57:07 +0000

    Keep only multiplication directory

--- misc/256_1M_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ./Transparent\ Huge\ Pages
-gcc -o intel-thp intel-thp.c -lgmp -o3
-sudo ./intel-thp 256 > thp-dump.txt
-
-cd ../Regular-Implementation
-gcc -o intel intel.c -lgmp -o3
-sudo ./intel 256 > dump.txt
