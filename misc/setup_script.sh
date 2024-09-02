216d0c93ae37ea4961680371f4aa7a1b3a268eef
lattu <lattu@node-0.digitsonturbo.isolateedinburgh-pg0.clemson.cloudlab.us>
2024-09-02 06:01:02 +0000

    commented out some parts of the setup script

--- misc/setup_script.sh
+++ misc/setup_script.sh
@@ -5,18 +5,18 @@ sudo apt install linux-tools-5.15.0-86-generic
 sudo apt update
 sudo apt-get install libgmp3-dev
 
-sudo apt-get install -y systemtap gcc
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C8CAB6595FDFF622
+# sudo apt-get install -y systemtap gcc
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C8CAB6595FDFF622
 
-sudo apt-get update
+# sudo apt-get update
 
-codename=$(lsb_release -c | awk  '{print $2}')
-sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
-deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
-deb http://ddebs.ubuntu.com/ ${codename}-security main restricted universe multiverse
-deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
-deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
-EOF
+# codename=$(lsb_release -c | awk  '{print $2}')
+# sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
+# deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
+# deb http://ddebs.ubuntu.com/ ${codename}-security main restricted universe multiverse
+# deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
+# deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
+# EOF
 
 sudo apt-get update
 sudo apt-get install linux-image-$(uname -r)-dbgsym
