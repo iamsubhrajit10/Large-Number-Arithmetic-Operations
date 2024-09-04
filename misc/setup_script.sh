6c4cef09586cd14da946a728d8eb058d332772fc
iamsubhrajit10 <iamsubhrajit10@gmail.com>
2024-09-04 05:56:32 +0000

    modifications in the setup script

--- misc/setup_script.sh
+++ misc/setup_script.sh
@@ -1,9 +1,9 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install linux-tools-common
-sudo apt install linux-tools-5.15.0-86-generic
-sudo apt update
-sudo apt-get install libgmp3-dev
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y linux-tools-common
+sudo apt install -y linux-tools-5.15.0-86-generic
+sudo apt update -y
+sudo apt-get install -y libgmp3-dev
 
 # sudo apt-get install -y systemtap gcc
 # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C8CAB6595FDFF622
@@ -18,9 +18,9 @@ sudo apt-get install libgmp3-dev
 # deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
 # EOF
 
-sudo apt-get update
-sudo apt-get install linux-image-$(uname -r)-dbgsym
-sudo apt-get install linux-image-$(uname -r)-dbg
+sudo apt-get update -y
+sudo apt-get install -y linux-image-$(uname -r)-dbgsym
+sudo apt-get install -y linux-image-$(uname -r)-dbg
 
 # // write shell command to increase the timeout time of ssh disconnetion
 # // Do the following
@@ -33,8 +33,11 @@ echo "ClientAliveCountMax 720" | sudo tee -a /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
 # // write shell command to install perf
-sudo apt-get install linux-tools-common linux-tools-generic linux-tools-`uname -r`
+sudo apt-get install -y linux-tools-common linux-tools-generic linux-tools-`uname -r`
 git config --global credential.helper 'cache --timeout=3600'
 # chmod +x git_push.sh
 # nohup ./git_push.sh > ~/nohup.out 2>&1 &
 # sudo chmod -R 777 .
+# Add git config user names and emails
+git config --global user.email "iamsubhrajit10@gmail.com"
+git config --global user.name "iamsubhrajit10"
